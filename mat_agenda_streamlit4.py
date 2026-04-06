d9823b1615dd74f981f1b33f5e3864e19fcf43f3
Matsuke-del <anlybox976@gmail.com>
2026-04-06 10:09:12 +0400

    Update mat_agenda_streamlit4.py

--- mat_agenda_streamlit4.py
+++ mat_agenda_streamlit4.py
@@ -6,7 +6,7 @@ from streamlit_calendar import calendar
 from supabase import create_client
 
 url = "https://quamffmaxqhhtyxworou.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1YW1mZm1heHFoaHR5eHdvcm91Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTM3MzgyNSwiZXhwIjoyMDkwOTQ5ODI1fQ.Q8TeZQvizrRNQWcPws_BbQwOXzvEt1H4_jxzwS0_qYU"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1YW1mZm1heHFoaHR5eHdvcm91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNzM4MjUsImV4cCI6MjA5MDk0OTgyNX0.aO5mv0jaOzFrj_yd8JtXDBrff0mwzsdPZpei_i3C_BM"
 supabase = create_client(url, key)
 
 
