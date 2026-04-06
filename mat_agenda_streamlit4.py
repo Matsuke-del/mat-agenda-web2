6592e2fbefcaebc2488c535ea5318cdce742cf08
Matsuke-del <anlybox976@gmail.com>
2026-04-06 19:54:47 +0400

    Update mat_agenda_streamlit4.py

--- mat_agenda_streamlit4.py
+++ mat_agenda_streamlit4.py
@@ -9,14 +9,6 @@ url = "https://quamffmaxqhhtyxworou.supabase.co"
 key = "sb_publishable_zKt7ObrIa8kkHXjlvhk4tw_SUetSTZG"
 supabase = create_client(url, key)
 
-supabase.table("agenda").insert({
-    "date": str(date),
-    "debut": debut.strftime("%H:%M:%S"),
-    "fin": fin.strftime("%H:%M:%S"),
-    "description": desc,
-    "color": color
-}).execute()
-
 st.set_page_config(layout="wide")
 st.title("🧠 MAT AGENDA TXT")
 
@@ -54,6 +46,10 @@ page=st.sidebar.radio(
 "Navigation",
 ["📅 Calendrier","📂 Liste","📊 Statistiques"]
 )
+
+# =========================
+# AJOUT ACTIVITE
+# =========================
 
 # =========================
 # AJOUT ACTIVITE
