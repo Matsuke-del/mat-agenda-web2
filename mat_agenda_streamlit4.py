fec2961830a6337f6c2a97523d92831f611772fc
Matsuke-del <anlybox976@gmail.com>
2026-04-07 13:44:26 +0400

    Update mat_agenda_streamlit4.py

--- mat_agenda_streamlit4.py
+++ mat_agenda_streamlit4.py
@@ -235,15 +235,16 @@ if page == "📂 Liste":
             with col1:
                 st.markdown(
                     f"""
-### {row['description']}
+            ### {row['description']}
 
-📅 {row['date']}
+            📅 {row['date']}
 
-⏰ {row['debut']} → {row['fin']}
+            ⏰ {row['debut']} → {row['fin']}
 
-⏱ {round(row['heures'], 2)} h
-"""
-                )
+             ⏱ {round(row['heures'], 2)} h
+             """
+             )
+
 
                 # afficher image si elle existe
                 if "image_url" in row and row["image_url"] and str(row["image_url"]).startswith("http"):
