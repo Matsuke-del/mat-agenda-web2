c3eb40a21e2a858977af394ce7f8ab957d030020
Matsuke-del <anlybox976@gmail.com>
2026-04-07 14:32:17 +0400

    Refactor and reformat mat_agenda_streamlit4.py

--- mat_agenda_streamlit4.py
+++ mat_agenda_streamlit4.py
@@ -244,8 +244,8 @@ Heure : {row['debut']} - {row['fin']}
 
 Durée : {round(row['heures'], 2)} h
 """
-                )
-            )    
+                ))
+           
                 # afficher image si elle existe
                 if "image_url" in row and row["image_url"] and str(row["image_url"]).startswith("http"):
                     st.image(row["image_url"], width=350)
