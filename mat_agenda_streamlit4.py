38cac8f6f75df123f118ff8b766b3f8ad7dfbd2c
Matsuke-del <anlybox976@gmail.com>
2026-04-07 13:49:07 +0400

    Update mat_agenda_streamlit4.py

--- mat_agenda_streamlit4.py
+++ mat_agenda_streamlit4.py
@@ -237,13 +237,13 @@ if page == "📂 Liste":
                     f"""
             ### {row['description']}
 
-            📅 {row['date']}
+                    📅 {row['date']}
 
-            ⏰ {row['debut']} → {row['fin']}
+                    ⏰ {row['debut']} → {row['fin']}
 
-             ⏱ {round(row['heures'], 2)} h
-             """
-             )
+                     ⏱ {round(row['heures'], 2)} h
+                     """
+                     )
 
 
                 # afficher image si elle existe
