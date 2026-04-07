ed650d3128399063311727e3c3d655a7d41e0d65
Matsuke-del <anlybox976@gmail.com>
2026-04-07 13:09:42 +0400

    Update mat_agenda_streamlit4.py

--- mat_agenda_streamlit4.py
+++ mat_agenda_streamlit4.py
@@ -208,7 +208,7 @@ if page=="📂 Liste":
 """)
 
                 # afficher image si elle existe
-                if "image_url" in row and row["image_url"]:
+                if "image_url" in row and row["image_url"] and str(row["image_url"]).startswith("http"):
                     st.image(row["image_url"], width=350)
 
             with col2:
